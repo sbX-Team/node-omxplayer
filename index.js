99b816ae88b363c6fef82f164397540f1427a50d
Jason Small <sops21@gmail.com>
2021-03-25 13:09:50 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,175 +1,158 @@
-'use strict';
+'use strict'
 
 // ----- Requires ----- //
 
-let spawn = require('child_process').spawn;
-let EventEmitter = require('events');
-
+const spawn = require('child_process').spawn
+const EventEmitter = require('events')
 
 // ----- Setup ----- //
 
 // The permitted audio outputs, local means via the 3.5mm jack.
-let ALLOWED_OUTPUTS = ['hdmi', 'local', 'both', 'alsa'];
-
+const ALLOWED_OUTPUTS = ['hdmi', 'local', 'both', 'alsa']
 
 // ----- Functions ----- //
 
 // Creates an array of arguments to pass to omxplayer.
-function buildArgs (source, givenOutput, loop, initialVolume, showOsd) {
-	let output = '';
+function buildArgs (source, givenOutput, loop, initialVolume, showOsd, rotation) {
+  let output = ''
 
-	if (givenOutput) {
+  if (givenOutput) {
+    // if (ALLOWED_OUTPUTS.indexOf(givenOutput) === -1) {
+    // throw new Error(`Output ${givenOutput} not allowed.`);
+    // }
 
-		// if (ALLOWED_OUTPUTS.indexOf(givenOutput) === -1) {
-			// throw new Error(`Output ${givenOutput} not allowed.`);
-		// }
+    output = givenOutput
+  } else {
+    output = 'local'
+  }
 
-		output = givenOutput;
+  let osd = false
+  if (showOsd) {
+    osd = showOsd
+  }
+  let osd = false
+  if(rotation){
+	rot = rotation
+  } else {
+	rot = 0
+  }
 
-	} else {
-		output = 'local';
-	}
+  const args = [source, '-o', output, '--blank',  '--rotation', rot, osd ? '' : '--no-osd',]
 
-	let osd = false;
-	if (showOsd) {
-		osd = showOsd;
-	}
+  // Handle the loop argument, if provided
+  if (loop) {
+    args.push('--loop')
+  }
 
-	let args = [source, '-o', output, '--blank', osd ? '' : '--no-osd'];
+  // Handle the initial volume argument, if provided
+  if (Number.isInteger(initialVolume)) {
+    args.push('--vol', initialVolume)
+  }
 
-	// Handle the loop argument, if provided
-	if (loop) {
-		args.push('--loop');
-	}
-
-	// Handle the initial volume argument, if provided
-	if (Number.isInteger(initialVolume)) {
-		args.push('--vol', initialVolume);
-	}
-
-	return args;
-
+  return args
 }
-
 
 // ----- Omx Class ----- //
 
 function Omx (source, output, loop, initialVolume, showOsd) {
+  // ----- Local Vars ----- //
 
-	// ----- Local Vars ----- //
+  const omxplayer = new EventEmitter()
+  let player = null
+  let open = false
 
-	let omxplayer = new EventEmitter();
-	let player = null;
-	let open = false;
+  // ----- Local Functions ----- //
 
-	// ----- Local Functions ----- //
+  // Marks player as closed.
+  function updateStatus () {
+    open = false
+    omxplayer.emit('close')
+  }
 
-	// Marks player as closed.
-	function updateStatus () {
+  // Emits an error event, with a given message.
+  function emitError (message) {
+    open = false
+    omxplayer.emit('error', message)
+  }
 
-		open = false;
-		omxplayer.emit('close');
+  // Spawns the omxplayer process.
+  function spawnPlayer (src, out, loop, initialVolume, showOsd, rotation) {
+    const args = buildArgs(src, out, loop, initialVolume, showOsd, rotation)
+    console.log('args for omxplayer:', args)
+    const omxProcess = spawn('omxplayer', args)
+    open = true
 
-	}
+    omxProcess.stdin.setEncoding('utf-8')
+    omxProcess.on('close', updateStatus)
 
-	// Emits an error event, with a given message.
-	function emitError (message) {
+    omxProcess.on('error', () => {
+      emitError('Problem running omxplayer, is it installed?.')
+    })
 
-		open = false;
-		omxplayer.emit('error', message);
+    return omxProcess
+  }
 
-	}
+  // Simulates keypress to provide control.
+  function writeStdin (value) {
+    if (open) {
+      player.stdin.write(value)
+    } else {
+      throw new Error('Player is closed.')
+    }
+  }
 
-	// Spawns the omxplayer process.
-	function spawnPlayer (src, out, loop, initialVolume, showOsd) {
+  // ----- Setup ----- //
 
-		let args = buildArgs(src, out, loop, initialVolume, showOsd);
-		console.log('args for omxplayer:', args);
-		let omxProcess = spawn('omxplayer', args);
-		open = true;
+  if (source) {
+    player = spawnPlayer(source, output, loop, initialVolume, showOsd)
+  }
 
-		omxProcess.stdin.setEncoding('utf-8');
-		omxProcess.on('close', updateStatus);
+  // ----- Methods ----- //
 
-		omxProcess.on('error', () => {
-			emitError('Problem running omxplayer, is it installed?.');
-		});
+  // Restarts omxplayer with a new source.
+  omxplayer.newSource = (src, out, loop, initialVolume, showOsd) => {
+    if (open) {
+      player.on('close', () => { player = spawnPlayer(src, out, loop, initialVolume, showOsd) })
+      player.removeListener('close', updateStatus)
+      writeStdin('q')
+    } else {
+      player = spawnPlayer(src, out, loop, initialVolume, showOsd)
+    }
+  }
 
-		return omxProcess;
+  omxplayer.play = () => { writeStdin('p') }
+  omxplayer.pause = () => { writeStdin('p') }
+  omxplayer.volUp = () => { writeStdin('+') }
+  omxplayer.volDown = () => { writeStdin('-') }
+  omxplayer.fastFwd = () => { writeStdin('>') }
+  omxplayer.rewind = () => { writeStdin('<') }
+  omxplayer.fwd30 = () => { writeStdin('\u001b[C') }
+  omxplayer.back30 = () => { writeStdin('\u001b[D') }
+  omxplayer.fwd600 = () => { writeStdin('\u001b[A') }
+  omxplayer.back600 = () => { writeStdin('\u001b[B') }
+  omxplayer.quit = () => { writeStdin('q') }
+  omxplayer.subtitles = () => { writeStdin('s') }
+  omxplayer.info = () => { writeStdin('z') }
+  omxplayer.incSpeed = () => { writeStdin('1') }
+  omxplayer.decSpeed = () => { writeStdin('2') }
+  omxplayer.prevChapter = () => { writeStdin('i') }
+  omxplayer.nextChapter = () => { writeStdin('o') }
+  omxplayer.prevAudio = () => { writeStdin('j') }
+  omxplayer.nextAudio = () => { writeStdin('k') }
+  omxplayer.prevSubtitle = () => { writeStdin('n') }
+  omxplayer.nextSubtitle = () => { writeStdin('m') }
+  omxplayer.decSubDelay = () => { writeStdin('d') }
+  omxplayer.incSubDelay = () => { writeStdin('f') }
 
-	}
+  Object.defineProperty(omxplayer, 'running', {
+    get: () => { return open }
+  })
 
-	// Simulates keypress to provide control.
-	function writeStdin (value) {
+  // ----- Return Object ----- //
 
-		if (open) {
-			player.stdin.write(value);
-		} else {
-			throw new Error('Player is closed.');
-		}
-
-	}
-
-	// ----- Setup ----- //
-
-	if (source) {
-		player = spawnPlayer(source, output, loop, initialVolume, showOsd);
-	}
-
-	// ----- Methods ----- //
-
-	// Restarts omxplayer with a new source.
-	omxplayer.newSource = (src, out, loop, initialVolume, showOsd) => {
-
-		if (open) {
-
-			player.on('close', () => { player = spawnPlayer(src, out, loop, initialVolume, showOsd); });
-			player.removeListener('close', updateStatus);
-			writeStdin('q');
-
-		} else {
-
-			player = spawnPlayer(src, out, loop, initialVolume, showOsd);
-
-		}
-
-	};
-
-	omxplayer.play = () => { writeStdin('p'); };
-	omxplayer.pause = () => { writeStdin('p'); };
-	omxplayer.volUp = () => { writeStdin('+'); };
-	omxplayer.volDown = () => { writeStdin('-'); };
-	omxplayer.fastFwd = () => { writeStdin('>'); };
-	omxplayer.rewind = () => { writeStdin('<'); };
-	omxplayer.fwd30 =() => { writeStdin('\u001b[C'); };
-	omxplayer.back30 = () => { writeStdin('\u001b[D'); };
-	omxplayer.fwd600 = () => { writeStdin('\u001b[A'); };
-	omxplayer.back600 = () => { writeStdin('\u001b[B'); };
-	omxplayer.quit = () => { writeStdin('q'); };
-	omxplayer.subtitles = () => { writeStdin('s'); };
-	omxplayer.info = () => { writeStdin('z'); };
-	omxplayer.incSpeed = () => { writeStdin('1'); };
-	omxplayer.decSpeed = () => { writeStdin('2'); };
-	omxplayer.prevChapter = () => { writeStdin('i'); };
-	omxplayer.nextChapter = () => { writeStdin('o'); };
-	omxplayer.prevAudio = () => { writeStdin('j'); };
-	omxplayer.nextAudio = () => { writeStdin('k'); };
-	omxplayer.prevSubtitle = () => { writeStdin('n'); };
-	omxplayer.nextSubtitle = () => { writeStdin('m'); };
-	omxplayer.decSubDelay = () => { writeStdin('d'); };
-	omxplayer.incSubDelay = () => { writeStdin('f'); };
-
-	Object.defineProperty(omxplayer, 'running', {
-		get: () => { return open; }
-	});
-
-	// ----- Return Object ----- //
-
-	return omxplayer;
-
+  return omxplayer
 }
-
 
 // ----- Module Exports ----- //
 
-module.exports = Omx;
+module.exports = Omx
